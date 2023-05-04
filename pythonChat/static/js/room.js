37a945f9506f3b839da81efd729a1974688a332d
Alberto Tomasin <892614@stud.unive.it>
2023-05-04 18:45:32 +0200

    message listening handled

--- pythonChat/static/js/room.js
+++ pythonChat/static/js/room.js
@@ -1,6 +1,24 @@
 // in questo modo mi connetto al server
 var socketio = io()
 
+const message = $('#messages')
+
+const createMessage = (name, msg) => {
+    const content = `
+        <div>
+            <span>
+                <strong>${name}</strong>: ${msg}
+            </span>
+        </div>
+    `
+    message.append(content)
+}
+
+// ascoltiamo l'evento message
+socketio.on('message', (data) => {
+    createMessage(data.name, data.message)
+})
+
 const sendMessage = () => {
-    console.log('messaggio')
+    console.log('msg')
 }
